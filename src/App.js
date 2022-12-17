5156672b9150f8932f3fed88ce32205d80df65f1
Ahmad Khidir <khidirahmad05@gmail.com>
2022-12-16 16:47:23 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import desktop from './assets/desktop.png'
+import mobile from './assets/mobile.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <a href='https://forms.gle/JtaZBL4QdBEda7ok6'>
+      <picture>
+        <source srcSet={desktop} media='(min-width: 450px)'></source>
+        <img src={mobile} />
+      </picture>
+    </a>
   );
 }
 
